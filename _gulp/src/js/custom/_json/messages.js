644219af9edc867fbd7e7e775c225e09d763c1e3
ponchobase <ponchobase@gmail.com>
2024-11-18 20:46:44 -0500

    update

--- _gulp/src/js/custom/_json/messages.js
+++ _gulp/src/js/custom/_json/messages.js
@@ -10,7 +10,6 @@ poncho_json.messages = {
         success: '<p><i class="fas fa-check font--green"></i> Poncho meme generator reset.</p>'
     },
     save: {
-        success: `<p><i class="fas fa-check font--green"></i> Poncho meme saved.</p>
-        <p class="font--small"><strong>NOTE:</strong> Click &nbsp;<i class="fa-solid fa-download"></i> to download your meme to your device. Alternatively, right-click (desktop) or hold (mobile) image for additional download options. Click &nbsp;<i class="fa-solid fa-pen"></i> to continue editing.</p>`
+        success: `<p><i class="fas fa-check font--green"></i> Poncho meme saved.</p>`
     }
 }
